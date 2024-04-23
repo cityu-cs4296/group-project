fe78148ab51362d3ac4126f89496e41e5936ead0
Kelvin Lam <>
2024-04-23 23:12:43 +0800

    chore(k8s): list out the resource utilization when deploying 1 / 5 / 10 pods

--- k8s/task2.ps1
+++ k8s/task2.ps1
@@ -1,2 +1,43 @@
-kubectl top pods -n kube-system
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Cleaning up deployment..."
+kubectl delete deployment task1-nginx-deployment
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Creating the 1 pod..."
+kubectl apply -f task1-1.yaml
+kubectl wait --for=condition=available deployment/task1-nginx-deployment --timeout=60s
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Sleep for 1 minute..."
+Start-Sleep -Seconds 60
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Checking the resource usage of the 1 pod..."
+kubectl top pods --all-namespaces
 kubectl top nodes
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Scaling the deployment to 5 replicas..."
+kubectl apply -f task1-5.yaml
+kubectl wait --for=condition=available deployment/task1-nginx-deployment --timeout=60s
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Sleep for 1 minute..."
+Start-Sleep -Seconds 60
+
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Checking the resource usage of the 5 pods..."
+kubectl top pods --all-namespaces
+kubectl top nodes
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Scaling the deployment to 10 replicas..."
+kubectl apply -f task1-10.yaml
+kubectl wait --for=condition=available deployment/task1-nginx-deployment --timeout=60s
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Sleep for 1 minute..."
+Start-Sleep -Seconds 60
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Checking the resource usage of the 10 pod..."
+kubectl top pods --all-namespaces
+kubectl top nodes
+
+Write-Output ""
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Cleaning up deployment..."
+kubectl delete deployment task1-nginx-deployment
--- k8s/task2.sh
+++ k8s/task2.sh
@@ -1,2 +1,44 @@
-kubectl top pods -n kube-system
+#!/bin/bash
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Cleaning up deployment..."
+kubectl delete deployment task1-nginx-deployment
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Creating the 1 pod..."
+kubectl apply -f task1-1.yaml
+kubectl wait --for=condition=available deployment/task1-nginx-deployment --timeout=60s
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Sleep for 1 minute..."
+sleep 60
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Checking the resource usage of the 1 pod..."
+kubectl top pods --all-namespaces
 kubectl top nodes
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Scaling the deployment to 5 replicas..."
+kubectl apply -f task1-5.yaml
+kubectl wait --for=condition=available deployment/task1-nginx-deployment --timeout=60s
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Sleep for 1 minute..."
+sleep 60
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Checking the resource usage of the 5 pods..."
+kubectl top pods --all-namespaces
+kubectl top nodes
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Scaling the deployment to 10 replicas..."
+kubectl apply -f task1-10.yaml
+kubectl wait --for=condition=available deployment/task1-nginx-deployment --timeout=60s
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Sleep for 1 minute..."
+sleep 60
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Checking the resource usage of the 10 pod..."
+kubectl top pods --all-namespaces
+kubectl top nodes
+
+echo ""
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Cleaning up deployment..."
+kubectl delete deployment task1-nginx-deployment
