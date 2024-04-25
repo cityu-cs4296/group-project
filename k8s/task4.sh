88444a7e0bd010d8f004a7d617ed36391d931aae
Kelvin Lam <>
2024-04-25 21:53:46 +0800

    fix(k8s): missing hostname in task4 script

--- k8s/task4.sh
+++ k8s/task4.sh
@@ -19,7 +19,7 @@ while true; do
 done
 
 # Start the locust load generator
-locust --headless -t 60 -u 100 -r 100 -f ../common/locust_file.py -H http://$hostname --only-summary --csv=./logs/task4 &
+locust --headless -t 60 -u 100 -r 100 -f ../common/locust_file.py -H http://$(kubectl get svc nginx-service -o jsonpath='{.status.loadBalancer.ingress[0].hostname}') --only-summary --csv=./logs/task4 &
 
 # Keep Monitoring the HPA to check when it scales the pods
 # Stop when the locust test is done
