cc8ea00756ec2928badcc60785ad4f2648afc40c
Kelvin Lam <>
2024-04-25 15:16:31 +0800

    fix(k8s): incorrect place to store locust log

--- k8s/task3.sh
+++ k8s/task3.sh
@@ -7,7 +7,7 @@ kubectl create configmap nginx-config --from-file=../common/nginx.conf --dry-run
 kubectl apply -f task3.yaml
 
 # Start the locust load generator
-locust --headless -t 60 -u 100 -r 100 -f ../common/locust_file.py -H http://$(kubectl get svc nginx-service -o jsonpath='{.status.loadBalancer.ingress[0].hostname}') --only-summary --csv=task3.k8s.out &
+locust --headless -t 60 -u 100 -r 100 -f ../common/locust_file.py -H http://$(kubectl get svc nginx-service -o jsonpath='{.status.loadBalancer.ingress[0].hostname}') --only-summary --csv=./logs/task3
 
 # Keep monitoring the nodes and pods resources utilization
 # Stop when the locust test is done
